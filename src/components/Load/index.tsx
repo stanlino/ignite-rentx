8f31f0c5e26197818678c80439d6552fccc9f57d
Stanley Filipe <stanley92filipe@gmail.com>
2022-02-20 10:35:26 -0300

    refactor the Car component

---
+++ src/components/Load/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { ActivityIndicator } from 'react-native'
+import { useTheme } from 'styled-components'
+
+export function Load(){
+    
+    const {colors} = useTheme()
+
+    return (
+        <ActivityIndicator  
+            color={colors.main}
+            size={'large'}
+            style={{ flex: 1 }}
+        />
+    )
+}
