0203b7b5d62156e56f2a5178f4039e657ff74a8a
Stanley Filipe <stanley92filipe@gmail.com>
2022-02-19 21:24:42 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler'
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
+import { StatusBar } from 'react-native';
+
+import { 
+  useFonts, 
+  Inter_400Regular,
+  Inter_500Medium,
+} from '@expo-google-fonts/inter'
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo'
+
+import theme from './src/styles/theme';
+
+import { Home } from './src/screens/Home';
+import { CarDetails } from './src/screens/CarDetails';
+import { Sheduling } from './src/screens/Sheduling';
+import { ShedulingDetails } from './src/screens/ShedulingDetails';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold
+  })
+
+  if (!fontsLoaded) return <AppLoading />
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <ThemeProvider theme={theme}>
+      <StatusBar />
+      <ShedulingDetails />
+    </ThemeProvider>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
