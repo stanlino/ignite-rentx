a2dc572b3455deba3b8c3b81e102f59a7ad3b357
Stanley Filipe <stanley92filipe@gmail.com>
2022-02-20 12:44:27 -0300

    add routes types

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
+import { createStackNavigator, StackScreenProps } from '@react-navigation/stack'
 
 import { Home } from '../screens/Home'
 import { CarDetails } from '../screens/CarDetails'
 import { Sheduling } from '../screens/Sheduling'
 import { ShedulingComplete } from '../screens/ShedulingComplete'
 import { ShedulingDetails } from '../screens/ShedulingDetails'
+import { CarDTO } from '../dtos/carDto'
 
 const { Navigator, Screen } = createStackNavigator()
+
+type RootStackParamList = {
+  Home: undefined
+  CarDetails: { car: CarDTO }
+  Sheduling: undefined
+  ShedulingComplete: undefined
+  ShedulingDetails: undefined
+};
+
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
 
 export function AppRoutes(){
   return (
